345751af104c18714522f3966e4974ac96fd23a7
Juan Treminio <jtreminio@gmail.com>
2018-02-23 12:53:05 -0600

    Adds yarn/webpack/encore support. I am young again!

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+require('bootstrap/scss/bootstrap.scss');
+require('../css/app.scss');
+
+var $ = require('jquery');
+require('bootstrap');
+
+$(document).ready(function() {
+});
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+    .addEntry('app', './assets/js/app.js')
+    .enableSassLoader()
+    .autoProvidejQuery()
+    .enableSourceMaps(!Encore.isProduction())
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+;
+
+module.exports = Encore.getWebpackConfig();
