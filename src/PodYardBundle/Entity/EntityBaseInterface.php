8770395ca8252e1bdf77cbc6ea1a32d2f7802297
Juan Treminio <jtreminio@gmail.com>
2018-02-26 00:31:29 -0600

    Adds project table; Apply Gedmo\Timestampable to created_at, updated_at

---
+++ src/PodYardBundle/Entity/EntityBaseInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace PodYardBundle\Entity;
+
+interface EntityBaseInterface
+{
+    public function getId();
+
+    public function getCreatedAt() : ?\DateTime;
+
+    public function setCreatedAt($created_at);
+
+    public function getUpdatedAt() : ?\DateTime;
+
+    public function setUpdatedAt($updated_at);
+}
--- src/PodYardBundle/Entity/User.php
+++ src/PodYardBundle/Entity/User.php
@@ -4,6 +4,7 @@ namespace PodYardBundle\Entity;
 
 use PodYardBundle\Util;
 
+use Doctrine\Common\Collections;
 use Doctrine\ORM\Mapping as ORM;
 use FOS\UserBundle\Model\User as BaseUser;
 
@@ -11,14 +12,54 @@ use FOS\UserBundle\Model\User as BaseUser;
  * @ORM\Entity
  * @ORM\Table(name="`user`")
  */
-class User extends BaseUser implements Util\HydratorInterface
+class User extends BaseUser implements Util\HydratorInterface, EntityBaseInterface
 {
     use Util\HydratorTrait;
+    use EntityBaseTrait;
 
     /**
      * @ORM\Id
-     * @ORM\Column(type="integer")
+     * @ORM\Column(name="id", type="integer")
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+
+    /**
+     * @ORM\OneToMany(targetEntity="PodYardBundle\Entity\Project", mappedBy="user")
+     */
+    protected $projects;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->projects = new Collections\ArrayCollection();
+    }
+
+    /**
+     * @param Project $project
+     * @return $this
+     */
+    public function addProject(Project $project)
+    {
+        $this->projects[] = $project;
+
+        return $this;
+    }
+
+    /**
+     * @param Project $project
+     */
+    public function removeProject(Project $project)
+    {
+        $this->projects->removeElement($project);
+    }
+
+    /**
+     * @return Project[]|Collections\ArrayCollection
+     */
+    public function getProjects()
+    {
+        return $this->projects;
+    }
 }
