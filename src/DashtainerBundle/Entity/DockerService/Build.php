95932a5e42d53a40c0d92afad52f59f163def901
Juan Treminio <jtreminio@gmail.com>
2018-02-28 20:08:07 -0600

    Adds new features

--- src/DashtainerBundle/Entity/DockerService/Build.php
+++ src/DashtainerBundle/Entity/DockerService/Build.php
@@ -9,6 +9,16 @@ class Build implements Util\HydratorInterface
     use Util\HydratorTrait;
 
     /**
+     * @see https://docs.docker.com/compose/compose-file/#args
+     */
+    protected $args = [];
+
+    /**
+     * @see https://docs.docker.com/compose/compose-file/#cache_from
+     */
+    protected $cache_from = [];
+
+    /**
      * @see https://docs.docker.com/compose/compose-file/#context
      */
     protected $context;
@@ -19,9 +29,68 @@ class Build implements Util\HydratorInterface
     protected $dockerfile;
 
     /**
-     * @see https://docs.docker.com/compose/compose-file/#args
+     * @see https://docs.docker.com/compose/compose-file/#labels
      */
-    protected $args = [];
+    protected $labels = [];
+
+    /**
+     * @see https://docs.docker.com/compose/compose-file/#shm_size
+     */
+    protected $shm_size;
+
+    /**
+     * @see https://docs.docker.com/compose/compose-file/#target
+     */
+    protected $target;
+
+    /**
+     * @param string      $key
+     * @param string|null $value
+     * @return $this
+     */
+    public function addArg(string $key, string $value = null)
+    {
+        $this->args[$key] = $value;
+
+        return $this;
+    }
+
+    public function getArgs() : array
+    {
+        return $this->args;
+    }
+
+    /**
+     * @param array $arr
+     * @return $this
+     */
+    public function setArgs(array $arr)
+    {
+        $this->args = $arr;
+
+        return $this;
+    }
+
+    public function removeArg(string $key)
+    {
+        unset($this->args[$key]);
+    }
+
+    public function getCacheFrom() : array
+    {
+        return $this->cache_from;
+    }
+
+    /**
+     * @param array $cache_from
+     * @return $this
+     */
+    public function setCacheFrom(array $cache_from)
+    {
+        $this->cache_from = $cache_from;
+
+        return $this;
+    }
 
     public function getContext() : ?string
     {
@@ -60,31 +129,63 @@ class Build implements Util\HydratorInterface
      * @param string|null $value
      * @return $this
      */
-    public function addArg(string $key, string $value = null)
+    public function addLabel(string $key, string $value = null)
     {
-        $this->args[$key] = $value;
+        $this->labels[$key] = $value;
 
         return $this;
     }
 
-    public function getArgs() : array
+    public function getLabels() : array
     {
-        return $this->args;
+        return $this->labels;
     }
 
     /**
      * @param array $arr
      * @return $this
      */
-    public function setArgs(array $arr)
+    public function setLabels(array $arr)
     {
-        $this->args = $arr;
+        $this->labels = $arr;
 
         return $this;
     }
 
-    public function removeArg(string $key)
+    public function removeLabel(string $key)
     {
-        unset($this->args[$key]);
+        unset($this->labels[$key]);
+    }
+
+    public function getShmSize() : ?string
+    {
+        return $this->shm_size;
+    }
+
+    /**
+     * @param string $shm_size
+     * @return $this
+     */
+    public function setShmSize(string $shm_size = null)
+    {
+        $this->shm_size = $shm_size;
+
+        return $this;
+    }
+
+    public function getTarget() : ?string
+    {
+        return $this->target;
+    }
+
+    /**
+     * @param string $target
+     * @return $this
+     */
+    public function setTarget(string $target = null)
+    {
+        $this->target = $target;
+
+        return $this;
     }
 }
