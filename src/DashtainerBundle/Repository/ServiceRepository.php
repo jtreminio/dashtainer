86d1fac85eb435a157c9cb40908f6eebfec5f6b9
Juan Treminio <jtreminio@gmail.com>
2018-02-28 18:17:11 -0600

    typehint findOneBy()

--- src/DashtainerBundle/Repository/ServiceRepository.php
+++ src/DashtainerBundle/Repository/ServiceRepository.php
@@ -2,8 +2,19 @@
 
 namespace DashtainerBundle\Repository;
 
+use DashtainerBundle\Entity;
+
 use Doctrine\ORM;
 
 class ServiceRepository extends ORM\EntityRepository
 {
+    /**
+     * @param array      $criteria
+     * @param array|null $orderBy
+     * @return null|Entity\Service
+     */
+    public function findOneBy(array $criteria, array $orderBy = null)
+    {
+        return parent::findOneBy($criteria, $orderBy);
+    }
 }
