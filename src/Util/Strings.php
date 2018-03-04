a06e1bdbf3dc9e84da24abc56dc2d4b9734cc859
Juan Treminio <jtreminio@gmail.com>
2018-03-04 16:11:52 -0600

    Adds hostname()

--- src/Util/Strings.php
+++ src/Util/Strings.php
@@ -8,4 +8,9 @@ abstract class Strings
     {
         return preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $string);
     }
+
+    public static function hostname(string $string) : string
+    {
+        return preg_replace("/[^a-zA-Z0-9\-]/", '-', $string);
+    }
 }
