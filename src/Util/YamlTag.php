1eb51a54c057f4d0d9220e38d883708014245350
Juan Treminio <jtreminio@gmail.com>
2018-03-11 16:29:07 -0500

    Adds emptyHash()

--- src/Util/YamlTag.php
+++ src/Util/YamlTag.php
@@ -5,12 +5,14 @@ namespace Dashtainer\Util;
 abstract class YamlTag
 {
     protected const DBL_QUOTES = '__SPECIAL_DBL_QUOTES__';
+    protected const EMPTY_HASH = '__SPECIAL_EMPTY_HASH';
     protected const NIL        = '__SPECIAL_NIL__';
 
     public static function parse(string $string) : string
     {
         $string = static::doubleQuotesParse($string);
         $string = static::nilValueParse($string);
+        $string = static::emptyHashParse($string);
 
         return $string;
     }
@@ -34,5 +36,15 @@ abstract class YamlTag
     protected static function nilValueParse(string $string) : string
     {
         return str_replace(static::NIL, '', $string);
+    }
+
+    public static function emptyHash() : string
+    {
+        return static::EMPTY_HASH;
+    }
+
+    protected static function emptyHashParse(string $string) : string
+    {
+        return str_replace(static::EMPTY_HASH, '{  }', $string);
     }
 }
