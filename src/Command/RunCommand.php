07c3115fcaa667f37fa7049e42572679575fd8ab
Juan Treminio <jtreminio@gmail.com>
2018-03-12 12:28:13 -0500

    Missed some old Docker handling stuff

--- src/Command/RunCommand.php
+++ src/Command/RunCommand.php
@@ -15,24 +15,6 @@ class RunCommand extends CommandAbstract
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $kernel = $this->getContainer()->get('kernel');
-
-        $baseDir    = $kernel->getRootDir() . '/..';
-        $outputDir  = $baseDir . '/docker-generated';
-        $configFile = $baseDir . '/var/dashtainer-config.yml';
-
-        exec(sprintf('rm -rf %s', escapeshellarg($outputDir)));
-        exec(sprintf('mkdir %s', escapeshellarg($outputDir)));
-
-        $manager = $this->getContainer()->get('dashtainer.docker.manager');
-        $manager->setDashtainerConfig(Yaml::parseFile($configFile))
-            ->generateArchive($outputDir);
-
-        file_put_contents(
-            "{$outputDir}/docker-compose.yml",
-            $manager->getDockerConfigYaml()
-        );
-
         return 0;
     }
 }
