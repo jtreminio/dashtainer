179658ad5d531eabb282320007d675852a52bb98
Juan Treminio <jtreminio@gmail.com>
2018-04-19 14:27:53 -0500

    Adds MailHog tests

---
+++ tests/Domain/Docker/ServiceWorker/MailHogTest.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace Dashtainer\Tests\Domain\Docker\ServiceWorker;
+
+use Dashtainer\Domain\Docker\ServiceWorker\MailHog;
+use Dashtainer\Entity;
+use Dashtainer\Form;
+use Dashtainer\Repository;
+
+use Doctrine\ORM;
+use PHPUnit\Framework\MockObject\MockObject;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class MailHogTest extends KernelTestCase
+{
+    /** @var Form\Docker\Service\MailHogCreate */
+    protected $form;
+
+    /** @var MockObject|Repository\Docker\Network */
+    protected $networkRepo;
+
+    /** @var Entity\Docker\Project */
+    protected $project;
+
+    /** @var Entity\Docker\Network */
+    protected $publicNetwork;
+
+    protected $seededPrivateNetworks = [];
+
+    /** @var MockObject|Repository\Docker\Service */
+    protected $serviceRepo;
+
+    /** @var Entity\Docker\ServiceType */
+    protected $serviceType;
+
+    /** @var MockObject|Repository\Docker\ServiceType */
+    protected $serviceTypeRepo;
+
+    /** @var MailHog */
+    protected $worker;
+
+    protected function setUp()
+    {
+        $em = $this->getMockBuilder(ORM\EntityManagerInterface::class)
+            ->getMock();
+
+        $this->networkRepo = $this->getMockBuilder(Repository\Docker\Network::class)
+            ->setConstructorArgs([$em])
+            ->getMock();
+
+        $this->serviceRepo = $this->getMockBuilder(Repository\Docker\Service::class)
+            ->setConstructorArgs([$em])
+            ->getMock();
+
+        $this->serviceTypeRepo = $this->getMockBuilder(Repository\Docker\ServiceType::class)
+            ->setConstructorArgs([$em])
+            ->getMock();
+
+        $this->project = new Entity\Docker\Project();
+        $this->project->setName('project-name');
+
+        $this->publicNetwork = new Entity\Docker\Network();
+
+        $this->project->addNetwork($this->publicNetwork);
+
+        $this->serviceType = new Entity\Docker\ServiceType();
+        $this->serviceType->setName('service-type-name');
+
+        $moduleMeta = new Entity\Docker\ServiceTypeMeta();
+        $moduleMeta->setName('modules')
+            ->setData(['default' => ['default_data'], 'available' => ['available_data']]);
+
+        $this->serviceType->addMeta($moduleMeta);
+
+        $this->form = new Form\Docker\Service\MailHogCreate();
+        $this->form->project = $this->project;
+        $this->form->type    = $this->serviceType;
+        $this->form->name    = 'service-name';
+
+        $this->worker = new MailHog($this->serviceRepo, $this->networkRepo, $this->serviceTypeRepo);
+
+        $this->seedProjectWithPrivateNetworks();
+
+        $this->networkRepo->expects($this->any())
+            ->method('getPublicNetwork')
+            ->will($this->returnValue($this->publicNetwork));
+    }
+
+    protected function seedProjectWithPrivateNetworks()
+    {
+        $privateNetworkA = new Entity\Docker\Network();
+        $privateNetworkA->setName('private-network-a');
+
+        $privateNetworkB = new Entity\Docker\Network();
+        $privateNetworkB->setName('private-network-b');
+
+        $privateNetworkC = new Entity\Docker\Network();
+        $privateNetworkC->setName('private-network-c');
+
+        $this->project->addNetwork($privateNetworkA)
+            ->addNetwork($privateNetworkB)
+            ->addNetwork($privateNetworkC);
+
+        $this->seededPrivateNetworks = [
+            'private-network-a' => $privateNetworkA,
+            'private-network-b' => $privateNetworkB,
+            'private-network-c' => $privateNetworkC,
+        ];
+    }
+
+    public function testCreateReturnsServiceEntity()
+    {
+        $this->networkRepo->expects($this->once())
+            ->method('getPrivateNetworks')
+            ->with($this->form->project)
+            ->will($this->returnValue([]));
+
+        $this->networkRepo->expects($this->once())
+            ->method('findByService')
+            ->will($this->returnValue([]));
+
+        $service = $this->worker->create($this->form);
+
+        $labels = $service->getLabels();
+
+        $this->assertSame($this->form->name, $service->getName());
+        $this->assertSame($this->form->type, $service->getType());
+        $this->assertSame($this->form->project, $service->getProject());
+
+        $this->assertEquals('mailhog/mailhog:latest', $service->getImage());
+
+        $expectedTraefikBackendLabel       = 'service-name';
+        $expectedTraefikDockerNetworkLabel = 'traefik_webgateway';
+        $expectedTraefikFrontendRuleLabel  = 'Host:service-name.localhost';
+        $expectedTraefikPortLabel          = '8025';
+
+        $this->assertEquals($expectedTraefikBackendLabel, $labels['traefik.backend']);
+        $this->assertEquals(
+            $expectedTraefikDockerNetworkLabel,
+            $labels['traefik.docker.network']
+        );
+        $this->assertEquals(
+            $expectedTraefikFrontendRuleLabel,
+            $labels['traefik.frontend.rule']
+        );
+        $this->assertEquals(
+            $expectedTraefikPortLabel,
+            $labels['traefik.port']
+        );
+    }
+}
